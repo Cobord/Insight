bef44cb48dae53b9f08700d48c2b733643be68d2
Ubuntu <ubuntu@ip-10-0-0-9.ec2.internal>
2019-02-04 15:40:40 +0000

    make elasticsearch index from the input file, finds filenames with any matching hashes

--- save_elastic_search2.py
+++ save_elastic_search2.py
@@ -3,19 +3,36 @@ import json
 
 es = Elasticsearch([{'host':'localhost','port':9200}])
 
-e1={"file_name": "Placeholder1.wav","hash1": 103,
-	"hash2": 290, "hash3": 308, "hash4": 3, "hash5": 0}
-e2={"file_name": "Placeholder2.wav","hash1": 103,
-        "hash2": 90, "hash3": 38, "hash4": 13, "hash5": 20}
-e3={"file_name": "Placeholder3.wav","hash1": 13,
-        "hash2": 29, "hash3": 3, "hash4": 93, "hash5": 200}
+#e1={"file_name": "Placeholder1.wav","hash1": 103,
+#	"hash2": 290, "hash3": 308, "hash4": 3, "hash5": 0}
+#e2={"file_name": "Placeholder2.wav","hash1": 103,
+#        "hash2": 90, "hash3": 38, "hash4": 13, "hash5": 20}
+#e3={"file_name": "Placeholder3.wav","hash1": 13,
+#        "hash2": 29, "hash3": 3, "hash4": 93, "hash5": 200}
 
-all_files=[(1,e1),(2,e2),(3,e3)]
+#all_files=[(1,e1),(2,e2),(3,e3)]
 
-for (index,file) in all_files:
-	es.index(index='insight',doc_type='wavHashes',id=index,body=file)
+#for (index,file) in all_files:
+#	es.index(index='insight',doc_type='wavHashes',id=index,body=file)
 
-print(es.get(index='insight',doc_type='wavHashes',id=2)['_source']['file_name'])
+#print(es.get(index='insight',doc_type='wavHashes',id=2)['_source']['file_name'])
+
+def make_es(from_file):
+	with open(from_file) as f:
+		for line in f.readlines():
+			[hash1,hash2,hash3,hash4,hash5,filename]=line.split(';')
+			hash1=int(hash1)
+			hash2=int(hash2)
+			hash3=int(hash3)
+			hash4=int(hash4)
+			hash5=int(hash5)
+			filename=filename.rstrip()
+			e={"file_name":filename,"hash1":hash1,
+				"hash2":hash2,"hash3":hash3,"hash4":hash4,
+				"hash5":hash5}
+			es.index(index='insight',doc_type='wavHashes',id=hash(filename),body=e)
+
+make_es('try_hashes.txt')
 
 def get_any_matches(hash1,hash2,hash3,hash4,hash5):
 	result1=es.search(index='insight',
@@ -37,8 +54,8 @@ def get_any_matches(hash1,hash2,hash3,hash4,hash5):
 	result5=es.search(index='insight',
                 body={'query':{'match':{'hash5': hash5}}})['hits']['hits']
         result5=map(lambda entry:entry['_source']['file_name'],result5)
-	
+
 	return set(result1+result2+result3+result4+result5)
 
-z=get_any_matches(103,0,0,0,0)
+z=get_any_matches(103,58,29,12,21)
 print(z)
