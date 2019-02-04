05106b56820a9f49de7dad3d91aef6c99fc59e8b
Ubuntu <ubuntu@ip-10-0-0-9.ec2.internal>
2019-02-04 14:10:38 +0000

    finds matches of any of the hashes

--- save_elastic_search2.py
+++ save_elastic_search2.py
@@ -1,4 +1,5 @@
 from elasticsearch import Elasticsearch
+import json
 
 es = Elasticsearch([{'host':'localhost','port':9200}])
 
@@ -14,6 +15,30 @@ all_files=[(1,e1),(2,e2),(3,e3)]
 for (index,file) in all_files:
 	es.index(index='insight',doc_type='wavHashes',id=index,body=file)
 
-print(es.get(index='insight',doc_type='wavHashes',id=2)['_source'])
+print(es.get(index='insight',doc_type='wavHashes',id=2)['_source']['file_name'])
 
+def get_any_matches(hash1,hash2,hash3,hash4,hash5):
+	result1=es.search(index='insight',
+		body={'query':{'match':{'hash1': hash1}}})['hits']['hits']
+	result1=map(lambda entry:entry['_source']['file_name'],result1)
 
+	result2=es.search(index='insight',
+                body={'query':{'match':{'hash2': hash2}}})['hits']['hits']
+        result2=map(lambda entry:entry['_source']['file_name'],result2)
+
+	result3=es.search(index='insight',
+                body={'query':{'match':{'hash3': hash3}}})['hits']['hits']
+        result3=map(lambda entry:entry['_source']['file_name'],result3)
+
+	result4=es.search(index='insight',
+                body={'query':{'match':{'hash4': hash4}}})['hits']['hits']
+        result4=map(lambda entry:entry['_source']['file_name'],result4)
+
+	result5=es.search(index='insight',
+                body={'query':{'match':{'hash5': hash5}}})['hits']['hits']
+        result5=map(lambda entry:entry['_source']['file_name'],result5)
+	
+	return set(result1+result2+result3+result4+result5)
+
+z=get_any_matches(103,0,0,0,0)
+print(z)
