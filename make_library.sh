bf60ff91e5ff2e1b175a2cbfea3cb34edd1ea746
Cobord <ammar.s.husain@gmail.com>
2019-02-08 21:00:57 +0000

    Comments to remind to make loops instead f fixed 10 or 5 locations

--- make_library.sh
+++ make_library.sh
@@ -9,6 +9,9 @@ hdfs dfs -rm -r /user/output6
 hdfs dfs -rm -r /user/output7
 hdfs dfs -rm -r /user/output8
 hdfs dfs -rm -r /user/output9
+
+# replace with loop so that can scale up
+
 rm try_hashes2.txt
 
 $SPARK_HOME/bin/spark-submit --master spark://ip-10-0-0-9:7077 --jars $SPARK_HOME/jars/elasticsearch-hadoop-2.1.0.Beta2.jar wav_to_hash5.py
@@ -18,6 +21,8 @@ hdfs dfs -cat /user/output1/part-00000 >> try_hashes2.txt
 hdfs dfs -cat /user/output2/part-00000 >> try_hashes2.txt
 hdfs dfs -cat /user/output3/part-00000 >> try_hashes2.txt
 hdfs dfs -cat /user/output4/part-00000 >> try_hashes2.txt
+
+# replace with loop to make sure scales
 
 $SPARK_HOME/bin/spark-submit --master spark://ip-10-0-0-9:7077 --jars $SPARK_HOME/jars/elasticsearch-hadoop-2.1.0.Beta2.jar save_elastic_search2.py
 
